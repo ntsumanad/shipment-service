b13eef01a52fc43c6fbc84b0a9a9b597bcc82530
ntsumanad <sumanadas.1990@gmail.com>
2022-10-25 18:54:01 -0700

    implemented Ishipment interface

--- src/org/edureka/shipping/IShipment.java
+++ src/org/edureka/shipping/IShipment.java
@@ -9,3 +9,4 @@ public interface IShipment {
 	public boolean getShipment();
 	
 }
+
---
+++ src/org/edureka/shipping/PackageShipment.java
@@ -0,0 +1,18 @@
+package org.edureka.shipping;
+
+public class PackageShipment implements IShipment {
+    @Override
+    public boolean addShipment() {
+        return false;
+    }
+
+    @Override
+    public boolean deleteShipment() {
+        return false;
+    }
+
+    @Override
+    public boolean getShipment() {
+        return false;
+    }
+}
